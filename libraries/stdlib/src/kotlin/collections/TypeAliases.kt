17b8cd243f635cae52b5e4fe36f2b8a31131d1a8
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2016-10-04 21:37:26 +0300

    Typealiases for jvm collections

---
+++ libraries/stdlib/src/kotlin/collections/TypeAliases.kt
@@ -0,0 +1,21 @@
+@file:kotlin.jvm.JvmVersion
+
+package kotlin.collections
+
+public typealias RandomAccess = java.util.RandomAccess
+public typealias Comparator<T> = java.util.Comparator<T> // kotlin.comparisons?
+
+
+// internal?
+public typealias ArrayList<E> = java.util.ArrayList<E>
+public typealias LinkedHashMap<K, V> = java.util.LinkedHashMap<K, V>
+public typealias HashMap<K, V> = java.util.HashMap<K, V>
+public typealias LinkedHashSet<E> = java.util.LinkedHashSet<E>
+public typealias HashSet<E> = java.util.HashSet<E>
+
+
+internal typealias Collections = java.util.Collections
+internal typealias Arrays = java.util.Arrays
+
+internal typealias SortedSet<E> = java.util.SortedSet<E>
+internal typealias TreeSet<E> = java.util.TreeSet<E>
